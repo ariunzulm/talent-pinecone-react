1c925029cf9ffd354d62626b0ef7228103c2275c
ariunzulm <munkhbatt.ariunzul@gmail.com>
2026-01-22 11:07:16 +0800

    started

---
+++ src/app/continue1/page1.jsx
@@ -0,0 +1,5 @@
+export default function ContinueOne(){
+    return (
+        <section>gitndgbet</section>
+    )
+}
---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+"use client";
+export default function Page() {
+  return (
+    <div>
+      <article>
+        <a href="/continue1"></a>
+      </article>
+    </div>
+  );
+}
